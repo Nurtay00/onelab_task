7e84e6bd784384175d67ee61c692decc74326b67
Nurtay00 <nurtay2000@gmail.com>
2020-07-27 19:23:04 +0600

    assd

--- index.js
+++ index.js
@@ -20,10 +20,10 @@ app.use(cors());
 app.use("/list", require("./routes/list"));
 app.use("/categories", require("./routes/category"));
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-  });
-}
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/build"));
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+//   });
+// }
 app.listen(port);

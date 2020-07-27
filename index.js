2c21310fc41170c573ec1be67d5e4188fc44276c
Nurtay00 <nurtay2000@gmail.com>
2020-07-27 19:13:35 +0600

    assd

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ mongoose.connect(
   }
 );
 app.use(express.json({ extended: true }));
-// app.use(express.urlencoded({ extended: true }));
-// app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 app.use("/list", require("./routes/list"));
 app.use("/categories", require("./routes/category"));

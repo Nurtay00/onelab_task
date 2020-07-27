f0c54e2f8b8c589d01ed60b70f653b20c39d6888
Nurtay00 <nurtay2000@gmail.com>
2020-07-27 19:26:39 +0600

    assd

--- index.js
+++ index.js
@@ -13,17 +13,17 @@ mongoose.connect(
     useCreateIndex: true,
   }
 );
-app.use(express.json({ extended: true }));
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 app.use("/list", require("./routes/list"));
 app.use("/categories", require("./routes/category"));
 
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static("client/build"));
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-//   });
-// }
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
+}
 app.listen(port);
